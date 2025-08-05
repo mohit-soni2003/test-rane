346dc53c8609c62d151d0dc78a16100511b21dfb
mohit-soni2003 <mohitsonip1847@gmail.com>
2025-08-05 16:54:33 +0530

    3rd commit

--- rane_project/server/keys.js
+++ rane_project/server/keys.js
@@ -2,7 +2,7 @@ module.exports = {
 
     // FRONTEND_ORIGIN_URL:"http://localhost:5173",
     SENDER_MAIL: "mohitsonip1847@gmail.com",
-    MONGO_URL:"mongodb+srv://mohit:Mohit123@cluster0.ruqg5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    // MONGO_URL:"mongodb+srv://mohit:Mohit123@cluster0.ruqg5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
     // MONGO_URL: "mongodb://localhost:27017/rane",
     MAIL_PASS: "hlyq uoeg rcyr sxtd",//mohit
 
@@ -12,7 +12,7 @@ module.exports = {
     // FRONTEND_ORIGIN_URL:"https://www.raneandsons.in",
     FRONTEND_ORIGIN_URL: "https://test-rane-cl.vercel.app", //version 30 test
 
-    // MONGO_URL:"mongodb+srv://ranesons11:SEycv5RPk65Y1xOY@cluster0.et6jd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    MONGO_URL:"mongodb+srv://ranesons11:SEycv5RPk65Y1xOY@cluster0.et6jd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
     // MAIL_PASS:"acby zvfn pury saoa",//rane
     // SENDER_MAIL:"noreply.rsems@gmail.com",
 
