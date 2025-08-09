1f34e372d0a38e4c0f3a069a6513de8de76184bf
mohit-soni2003 <mohitsonip1847@gmail.com>
2025-08-10 00:26:49 +0530

    dfd

--- rane_project/server/keys.js
+++ rane_project/server/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-    FRONTEND_ORIGIN_URL:"http://localhost:5173",
+    // FRONTEND_ORIGIN_URL:"http://localhost:5173",
     SENDER_MAIL: "mohitsonip1847@gmail.com",
     // MONGO_URL:"mongodb+srv://mohit:Mohit123@cluster0.ruqg5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
     // MONGO_URL: "mongodb://localhost:27017/rane",
@@ -16,7 +16,7 @@ module.exports = {
     // MAIL_PASS:"acby zvfn pury saoa",//rane
     // SENDER_MAIL:"noreply.rsems@gmail.com",
 
-    jwt_secret: "Mohit1234",
+    jwt_secret: "Mohit1234", 
 }
 // SENDER_MAIL:"ranesons11@gmail.com",
 // MAIL_PASS:"peli uazp ttpj ejya",//rane  
